191f058b88a230587f56abe76c9e9760f8f87b0a
Sharvesh83 <sharv.6183chola@gmail.com>
2026-01-08 09:39:05 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/saas-page/", // TODO: Update this if your repository name is different
 })
