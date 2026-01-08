6c9b38e9748faea12bb5fbbc9184f84e91c2b258
Sharvesh83 <116168311+Sharvesh83@users.noreply.github.com>
2026-01-08 09:57:56 +0530

    Fix vite base path for Netlify deployment - use root path instead of /static-saas-page/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/static-saas-page/", // TODO: Update this if your repository name is different
-})
+    base: "/",
+  })
+}
